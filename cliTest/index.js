1f37d5362878835e486825dd25fb321f994fa215
xuebigshuai <1499115658@qq.com>
2021-01-13 21:59:45 +0800

    [新增][cli][新增xyscli]

--- cliTest/index.js
+++ cliTest/index.js
@@ -1,8 +1,20 @@
 #!/usr/bin/env node
 
+//可以自动的解析命令和参数，用于处理用户输入的命令
 const program = require('commander');
+//下载并提取 git 仓库，用于下载项目模板
 const download = require('download-git-repo');
+//通用的命令行用户界面集合，用于和用户进行交互
 const inquirer = require('inquirer');
+const fs = require('fs');
+// 模板引擎，将用户提交的信息动态填充到文件中
+const handlebars = require('handlebars');
+//视觉美化
+const ora = require('ora');
+// 在信息前面加上 √ 或 × 等的图标
+const symbols = require('log-symbols');
+// chalk 来为打印信息加上样式
+const chalk = require('chalk');
 program.version('1.0.0', '-v', '--version')
        .command('init <name>')
        .action((name) => {
@@ -17,8 +29,29 @@ program.version('1.0.0', '-v', '--version')
             message:'请输入项目描述'
           }
         ]).then((answers) => {
-          download('https://github.com:xuebigshuai/template.git#master', name, {clone: true},err => {
-            console.log(err ? console.log(err) : 'Success')
+          const spinner = ora('正在下载模板....');
+          spinner.start();
+          download('https://github.com:xuebigshuai/template#main', name, {clone: true},err => {
+          if(err) {
+            spinner.fail();
+            console.log(symbols.error,chalk.red(err))
+          } else {
+            const fileName = `${name}/package.json`;
+            const meta = {
+              name,
+              description: answers.description,
+              author: answers.author
+            } 
+            if(fs.existsSync(fileName)) {
+              const content = fs.readFileSync(fileName).toString();
+              const result = handlebars.compile(content)(meta);
+              fs.writeFileSync(fileName, result);
+            }
+            console.log(symbols.success, chalk.green('项目初始化完成')); 
+            spinner.succeed('下载完成');
+          }
+          
+          
         })
         })
        });
